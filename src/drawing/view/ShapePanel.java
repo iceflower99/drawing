280116806b13b4d414de4c0d30fa46ddb493de99
iceflower99 <mhal4767@740s-pl-1280.local>
2016-04-26 08:34:14 -0600

    worked on arrayLists and ShapePanel()

--- src/drawing/view/ShapePanel.java
+++ src/drawing/view/ShapePanel.java
@@ -8,8 +8,21 @@ import java.util.ArrayList;
 public class ShapePanel extends JPanel
 {
 	private ArrayList<Rectangle> rectangleList;
-	private ArrayList<Square> squareList;
-	private ArrayList<>
+	private ArrayList<Rectangle> squareList;
+	private ArrayList<Ellipse2D> ellipseList;
+	private ArrayList<Ellipse2D> circleList;
+	private ArrayList<Polygon> triangleList;
+	private ArrayList<Polygon> polygonList;
 	
+	public ShapePanel()
+	{
+		triangleList = new ArrayList<Polygon>();
+		polygonList = new ArrayList<Polygon>();
+		rectangleList = new ArrayList<Rectangle>();
+		squareList = new ArrayList<Rectangle>();
+		circleList = new ArrayList<Ellipse2D>();
+		ellipseList = new ArrayList<Ellipse2D>(); 
+				
+	}
 	
 }
